318eea9739cd7b10ea6a760219d55d1da922607b
axar02 <ivannoorazhar@gmail.com>
2023-05-28 17:15:04 +0700

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  const response = await fetch("https://satudata.kulonprogokab.go.id/opendata/index.php/resources/detil/data-jumlah-balita-stunting-di-kabupaten-kulon-progo/df7ff80f-a25f-4631-92f2-8f8bd554ba40?to=api");
+  const response = await fetch("https://katalog.satudata.go.id/api/3/action/datastore_search?resource_id=9b741503-2e65-41d3-a0cf-51bdd7ee9677");
   res.json(await response.json());
 });
 
